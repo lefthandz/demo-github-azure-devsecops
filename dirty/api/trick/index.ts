523f932ee62c7cf47b28bbbe25e331ba59571d4d
Kazumi IWANAGA(OHIRA) / @dz_ <dzeyelid@gmail.com>
2023-03-17 06:45:23 +0000

    Update dirty api

--- dirty/api/trick/index.ts
+++ dirty/api/trick/index.ts
@@ -1,15 +1,26 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from '@azure/functions'
+import { CosmosClient } from '@azure/cosmos'
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
   context.log('HTTP trigger function processed a request.');
-  const name = (req.query.name || (req.body && req.body.name));
-  const responseMessage = name
-    ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+  // Azure Cosmos DB へ接続する
+  const endpoint = process.env.COSMOS_DB_ENDPOINT;
+  const key = process.env.COSMOS_DB_KEY;
+  const client = new CosmosClient({ endpoint, key });
+  const database = await client.database('dirty');
+  const container = await database.container('trick');
+
+  // ユーザーからのリクエストのクエリを Azure Cosmos DB で実行する
+  const query = req.query.query;
+  await container.items.query(query);
 
   context.res = {
     // status: 200, /* Defaults to 200 */
-    body: responseMessage
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: {}
   };
 };
 
