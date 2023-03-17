aaa83bb99581fdc3a8467c7ded932b4a2d182681
Kazumi IWANAGA(OHIRA) / @dz_ <dzeyelid@gmail.com>
2023-03-17 01:18:33 +0000

    Update dirty app

---
+++ dirty/app/components/vulnerable.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>This compornent is vulnerable😝</h1>
+    <div>
+      <form>
+        <input
+          type="text"
+          name="id"
+        >
+        <input
+          type="text"
+          name="message"
+        >
+        <input
+          type="submit"
+          value="submit"
+        >
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const isloop = true;
+  while(isloop) {
+    console.log('I am vulnerable!')
+  }
+</script>
