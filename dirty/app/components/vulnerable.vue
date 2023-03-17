0db6078c36a1bacc87940d403f806159fd03a4d8
Kazumi IWANAGA(OHIRA) / @dz_ <dzeyelid@gmail.com>
2023-03-17 02:49:22 +0000

    Update dirty app

--- dirty/app/components/vulnerable.vue
+++ dirty/app/components/vulnerable.vue
@@ -1,28 +1,43 @@
 <template>
-  <div>
+  <div class="container mx-auto">
     <h1>This compornent is vulnerable😝</h1>
     <div>
       <form>
-        <input
-          type="text"
-          name="id"
-        >
-        <input
-          type="text"
-          name="message"
-        >
-        <input
-          type="submit"
-          value="submit"
-        >
+        <div>
+          input:
+          <input
+            v-model="inputScript"
+            type="textarea"
+            name="script"
+          >
+        </div>
+        <div>
+          <input
+            type="submit"
+            value="Replace innerHtml"
+            @click.prevent="replace"
+          >
+        </div>
       </form>
+      <div
+        id="output"
+        style="border: 1px black solid"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  const isloop = true;
-  while(isloop) {
-    console.log('I am vulnerable!')
+  // const isloop = true;
+  // while(isloop) {
+  //   console.log('I am vulnerable!')
+  // }
+
+  const inputScript = ref('')
+  const replace = () => {
+    const output = document.getElementById('output')
+    if (output) {
+      output.innerHTML = inputScript.value
+    }
   }
 </script>
