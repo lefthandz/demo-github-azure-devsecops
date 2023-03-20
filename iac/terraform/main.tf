cc1206e22110a05a6dc219a31185c6a095e4bc21
Kazumi IWANAGA(OHIRA) / @dz_ <dzeyelid@gmail.com>
2023-03-20 11:10:41 +0000

    Update terraform code

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -18,15 +18,15 @@ resource "azurerm_resource_group" "main" {
 
 resource "azurerm_static_site" "main" {
   name                = "stapp-${var.workload}"
-  location            = var.staticWebApplocation
   resource_group_name = azurerm_resource_group.main.name
+  location            = var.staticWebApplocation
   sku_size            = var.staticWebAppSku
 }
 
 resource "azurerm_cosmosdb_account" "name" {
   name                = "cosmon-${var.workload}"
-  location            = var.location
   resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
   offer_type          = "Standard"
 
   capabilities {
@@ -41,4 +41,13 @@ resource "azurerm_cosmosdb_account" "name" {
     location          = var.location
     failover_priority = 0
   }
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "st${random_string.func_storage.result}"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_0"
 }
