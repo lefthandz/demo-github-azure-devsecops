d22f48319dd4c66b634e42cee7bba2cb12345096
Kazumi IWANAGA(OHIRA) / @dz_ <dzeyelid@gmail.com>
2023-03-21 00:56:31 +0900

    Update vulnerable-code-by-bing-chat.js

--- dirty/vulnerable-code-by-bing-chat.js
+++ dirty/vulnerable-code-by-bing-chat.js
@@ -1,24 +1,15 @@
 // DOM-based XSS
-import javascript
-
-from DataFlow::Node source, DataFlow::Node sink
-where source = DOM::source()
-  and sink = DOM::sink()
-  and source.flowsTo(sink)
-select sink, "This DOM write is vulnerable to XSS."
+var input = document.getElementById("user-input").value; // get user input from a text field
+document.getElementById("output").innerHTML = input; // write user input to a div element
 
 // RCE via eval
-import javascript
-
-from CallExpr call
-where call.getCalleeName() = "eval"
-select call, "This call to eval is potentially unsafe."
+var input = document.getElementById("user-input").value; // get user input from a text field
+eval(input); // execute user input as code
 
 // Path manipulation via fs module
-import javascript
-
-from DataFlow::SourceNode source, DataFlow::Node sink, DataFlow::Edge edge
-where source = RemoteFlowSource()
-  and sink = ModuleImportNode::moduleImport("fs")
-  and edge.getPath(source,sink).length() < 5 // limit path length for performance reasons
-select edge.getNode(0), "This input may be used to manipulate paths in the file system."
+var fs = require("fs"); // import file system module
+var input = process.argv[2]; // get user input from command line argument
+fs.readFile(input, "utf8", function(err, data) { // read file from user input path
+  if (err) throw err;
+  console.log(data); // print file content to console
+});
